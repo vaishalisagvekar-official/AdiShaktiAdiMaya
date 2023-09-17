88337b358c409b3c1fb7abb8ac3c79f3e274e47b
Vaishali Sagvekar <vaishalisagvekar20@gmail.com>
2023-09-18 01:20:45 +0530

    new build of improved design

---
+++ docs/assets/js/common.js
@@ -0,0 +1,33 @@
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     console.log("document ready");
+//     $(window).on("scroll", function() {
+//         console.log("window scroll");
+
+//         if($(window).scrollTop() > 50) {
+//           console.log("greater 5o");
+
+//             $(".header").addClass("active");
+//         } else {
+//           console.log("less 50");
+
+//             //remove the background property so it comes transparent again (defined in your css)
+//            $(".header").removeClass("active");
+//         }
+//     });
+// });
+
+$(function () {
+  var objTable = document.getElementById("whole-content");
+  objTable.addEventListener(
+    "scroll",
+    function () {
+      if ($(objTable).scrollTop() > 56) {
+        $("#navbar").addClass("whiteBg");
+      } else {
+        //remove the background property so it comes transparent again (defined in your css)
+        $("#navbar").removeClass("whiteBg");
+      }
+    },
+    false
+  );
+});
